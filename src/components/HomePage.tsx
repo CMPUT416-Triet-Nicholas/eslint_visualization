3f3eb9853b8ccc4848804a775b4165aa620c2a83
phamhuutriet <triet.ph1999@gmail.com>
2023-11-28 15:24:17 -0700

    feat: add basic bar chart

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Grid, LinearProgress } from "@mui/material";
+import { Bar } from "react-chartjs-2";
+import "chart.js/auto";
 
 export const HomePage = () => {
   return (
@@ -10,14 +12,18 @@ export const HomePage = () => {
         alignItems: "center",
       }}
     >
-      <Grid
-        container
-        sx={{
-          maxWidth: "1000px",
-        }}
-      >
-        <Grid item xs={12} sx={{ height: "100px", background: "#8098B8" }}>
-          <h4>THIS IS THE HOME PAGE</h4>
+      <Grid container sx={{ maxWidth: "80%" }}>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            height: "100px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>HOME PAGE</h1>
         </Grid>
         <Grid
           item
@@ -71,6 +77,48 @@ export const HomePage = () => {
             </Grid>
           </Grid>
         </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            height: "400px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid
+              item
+              xs={6}
+              sx={{
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <div style={{ height: "300px", width: "100%" }}>
+                {" "}
+                <HorizontalBarChart />
+              </div>
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              sx={{
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <div style={{ height: "300px", width: "100%" }}>
+                {" "}
+                <HorizontalBarChart />
+              </div>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
     </div>
   );
@@ -103,6 +151,59 @@ const StatisticBox = ({
         style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "10px" }}
       >{`${statNum}%`}</div>
       <LinearProgress variant="determinate" value={statNum} />
+    </div>
+  );
+};
+
+const HorizontalBarChart = () => {
+  const data = {
+    labels: [
+      "Item 1",
+      "Item 2",
+      "Item 3",
+      "item 4",
+      "item 5",
+      "item 6",
+      "item 7",
+      "item 7",
+      "item 7",
+      "item 7",
+    ],
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [65, 59, 80, 90, 100, 100, 100, 50, 50, 50],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const barHeight = 50;
+  const barSpacing = 10;
+  const chartHeight = data.labels.length * (barHeight + barSpacing);
+
+  const options: any = {
+    indexAxis: "y",
+    scales: {
+      x: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return (
+    <div style={{ height: `${chartHeight}px`, width: "90%" }}>
+      <Bar data={data} options={options} />
     </div>
   );
 };
